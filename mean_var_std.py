4127549bf2eacbace5de3a1a630160b6391e83fa
rinatmda <rinat.mukhametianov@alum.uca.es>
2026-01-18 16:15:09 +0000

    Add Project 1 with requirements.txt

--- mean_var_std.py
+++ mean_var_std.py
@@ -1,8 +1,23 @@
 import numpy as np
 
 def calculate(list):
-
-
-
+    if len(list) == 9:
+        x = np.array(list).reshape(3,3)
+        method = {'mean': x.mean,
+                'variance': x.var,
+                'standard deviation': x.std,
+                'max': x.max,
+                'min': x.min,
+                'sum': x.sum}
+        axes = [0, 1, None]
+        calculations = {} 
+        for m in method:
+            results_for_method = []
+            for a in axes:
+                result = method[m](axis = a).tolist()  #calculation for given statistical method m and given axis a
+                results_for_method.append(result)   
+            calculations[m] = results_for_method    
+    else:
+        raise ValueError('List must contain nine numbers.')
 
     return calculations
